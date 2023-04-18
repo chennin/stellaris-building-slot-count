1986aff151bff5c902dd58afb4508797b04528a9
forbiddenlake <saturos@gmail.com>
2023-04-18 08:09:09 -0400

    Fix survey sorting

--- survey-vanilla.sh
+++ survey-vanilla.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find ~/stellaris-game/ -type f -name "*.txt" -exec ./look_for_add.py '{}' \; | sort -k 2,3 | column -t | tee vanilla_survey.txt
+find ~/stellaris-game/ -type f -name "*.txt" -exec ./look_for_add.py '{}' \; | sort -k 1,2 | column -t | tee vanilla_survey.txt
