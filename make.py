b1bfe766317d8f4789fa93527585f2aafd52f10e
forbiddenlake <2653277+chennin@users.noreply.github.com>
2023-10-17 07:43:20 -0400

    3.9.3 update - no changes

--- make.py
+++ make.py
@@ -31,7 +31,7 @@ cwp.vanilla_path = os.path.expanduser( os.path.expandvars( "~/stellaris-game" ) 
 
 MOD_NAME = "Show Building Slot Capacity [beta for 3.9]"
 VERSION = "5"
-SUPPORTED_VERSION = "3.9.2"
+SUPPORTED_VERSION = "3.9.3"
 # 3 = unlisted, 2 = hidden, 1 = friends, 0 = public
 VISIBILITY = 0
 
--- mod_BPV/descriptor.mod
+++ mod_BPV/descriptor.mod
@@ -1,6 +1,6 @@
 name="Show Building Slot Capacity [beta for 3.9] - BPV"
 version="5"
-supported_version="3.9.2"
+supported_version="3.9.3"
 tags={
         "Graphics"
 }
